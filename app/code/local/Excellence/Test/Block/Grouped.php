916fa0f77839eb53aaffcda7c239e2b5a91c2bf1
unknown <neginn@SCOPC65.scoefx.com>
2016-06-03 09:55:07 -0700

    first commit

---
+++ app/code/local/Excellence/Test/Block/Grouped.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Excellence_Test_Block_Grouped extends Mage_Catalog_Block_Product_View_Type_Grouped{
+
+
+    
+}
