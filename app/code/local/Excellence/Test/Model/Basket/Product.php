b88a50abbb04d6f0a93ac94580558f79b2829a74
unknown <neginn@SCOPC65.scoefx.com>
2016-06-07 14:03:39 -0700

    commit all;

--- app/code/local/Excellence/Test/Model/Basket/Product.php
+++ app/code/local/Excellence/Test/Model/Basket/Product.php
@@ -1,14 +1,6 @@
 <?php
 
-class Excellence_Test_Model_Basket_Product extends Mage_Catalog_Model_Product_Type_Abstract
+class Excellence_Test_Model_Basket_Product extends Mage_Catalog_Model_Product_Type_Grouped 
 {
-    public function isComposite($product = null)
-    {
-        return true;
-    }
-   public function setTypeId($typeId)
-    {
-        
-        return Mage_Catalog_Model_Product_Type::TYPE_GROUPED;
-    }
+  
 }
